53f7e6c6dff2ad7ddf2ab5743ff5393d840d5d30
Mehtab Singh Jalaf <77369363+mehtabjalaf@users.noreply.github.com>
2024-09-19 18:20:55 -0400

    Initial commit

---
+++ FormsFunc.js
@@ -0,0 +1,10 @@
+// put your JS functions here
+
+function ....  // validate various form components{
+	
+
+function ....   // toggle logo
+function ....   // change background
+function ...
+   
+ 
---
+++ README.md
@@ -0,0 +1,2 @@
+# 4413Lab01
+ learning basic html, css, and js
