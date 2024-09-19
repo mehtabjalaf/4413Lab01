735590a661ebea6cbb26b98e2a7a865e9fd06ef4
Mehtab Singh Jalaf <77369363+mehtabjalaf@users.noreply.github.com>
2024-09-19 18:23:48 -0400

    feat: reduced code smell for turnlighton

--- light.js
+++ light.js
@@ -1,40 +1,5 @@
 /* 4413 24f Task 1 - write your code here */
 //alert("Test");
-function lightOneOn() {
-  /* set image src to be that for light 1, and set message texts to be "light #1 is on"  */
-  var aV = document.getElementById("light");
-  aV.src = "images/light/light_1.jpg";
-  document.getElementById("message").innerHTML = "light #1 is on";
-}
-
-function lightTwoOn() {
-  /* set image src to be that for light 1, and set message texts to be "light #1 is on"  */
-  var aV = document.getElementById("light");
-  aV.src = "images/light/light_2.jpg";
-  document.getElementById("message").innerHTML = "light #2 is on";
-}
-
-function lightThreeOn() {
-  /* set image src to be that for light 1, and set message texts to be "light #1 is on"  */
-  var aV = document.getElementById("light");
-  aV.src = "images/light/light_3.jpg";
-  document.getElementById("message").innerHTML = "light #3 is on";
-}
-
-function lightFourOn() {
-  /* set image src to be that for light 1, and set message texts to be "light #1 is on"  */
-  var aV = document.getElementById("light");
-  aV.src = "images/light/light_4.jpg";
-  document.getElementById("message").innerHTML = "light #4 is on";
-}
-
-function lightFiveOn() {
-  /* set image src to be that for light 1, and set message texts to be "light #1 is on"  */
-  var aV = document.getElementById("light");
-  aV.src = "images/light/light_5.jpg";
-  document.getElementById("message").innerHTML = "light #5 is on";
-}
-
 function turnLightOn(lightNumber) {
   var aV = document.getElementById("light");
   aV.src = "images/light/light_" + lightNumber + ".jpg";
